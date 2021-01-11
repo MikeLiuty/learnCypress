41104d8297f4c641cc8661971c704b60f7effa4d
Mike Liu <mikeliuty@gmail.com>
2021-01-12 00:12:05 +1300

    Type changes

--- cypress/integration/homework_1/simpleEquation.spec.js
+++ cypress/integration/homework_1/simpleEquation.spec.js
@@ -2,18 +2,18 @@
 
 describe('Positive- numbers ', () => {
     it('10 should be a number', () => {
-      expect(10).to.not.be.a("string")
+      expect(10).to.be.a("number")
     })
   })
 
   describe('Nagetive- Strings', () => {
     it('15 should not be a string', () => {
-      expect(10).to.not.be.a("string")
+      expect(15).to.not.be.a("string")
     })
   })
 
 describe('10 plus 15', () => {
-    it('10 plus 15 should eqaute to 25', () => {
+    it('10 plus 15 should equal to 25', () => {
       expect(10+15).to.equal(25)
     })
   })
