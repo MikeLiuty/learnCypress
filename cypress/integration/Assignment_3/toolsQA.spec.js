44454ec71ec9951ce82895047b51725ba173e3b0
Mike Liu <mikeliuty@gmail.com>
2021-01-19 21:33:54 +1300

    Assignment 3

--- cypress/integration/Assignment_3/toolsQA.spec.js
+++ cypress/integration/Assignment_3/toolsQA.spec.js
@@ -29,11 +29,39 @@ context ('Actions', () =>{
             cy.get('#userEmail')
             .clear()
             .type(profileData.profile_info.Email);
-            cy.get('input[value="Female"]').click({force: true})
+            cy.get('div').contains(JSON.stringify(profileData.profile_info.Gender).replace(/\"/g, "")).click({force: true})
             cy.get('#userNumber')
             .clear()
             .type(profileData.profile_info.Mobile);
-//            cy.get('#stateCity-wrapper').contains('NCR').click()
+            //I noticed the above steps can be implement by using keyboard as well. 
+            cy.get('#state').click()
+            //cy.get('.css-26l3qy-menu').contains('NCR').click()
+            cy.get('.css-26l3qy-menu').contains(JSON.stringify(profileData.profile_info.State).replace(/\"/g, "")).click() 
+            cy.get('#city').click()
+            cy.get('.css-26l3qy-menu').contains(JSON.stringify(profileData.profile_info.City).replace(/\"/g, "")).click()
+            cy.get('#submit').click()
+
+            cy.get('tbody tr').eq(0)
+                .should('contain','Student Name')
+                .should('contain',JSON.stringify(profileData.profile_info.firstName).replace(/\"/g, ""))
+                .should('contain',JSON.stringify(profileData.profile_info.lastName).replace(/\"/g, ""))
+            
+            cy.get('tbody tr').eq(1)
+                .should('contain','Student Email')
+                .should('contain',JSON.stringify(profileData.profile_info.Email).replace(/\"/g, ""))
+
+            cy.get('tbody tr').eq(2)
+                .should('contain','Gender')
+                .should('contain',JSON.stringify(profileData.profile_info.Gender).replace(/\"/g, ""))
+
+            cy.get('tbody tr').eq(3)
+                .should('contain','Mobile')
+                .should('contain',JSON.stringify(profileData.profile_info.Mobile).replace(/\"/g, ""))
+
+            cy.get('tbody tr').eq(9)
+                .should('contain','State and City')
+                .should('contain',JSON.stringify(profileData.profile_info.State).replace(/\"/g, ""))
+                .should('contain',JSON.stringify(profileData.profile_info.City).replace(/\"/g, ""))
         })
     })
 })
